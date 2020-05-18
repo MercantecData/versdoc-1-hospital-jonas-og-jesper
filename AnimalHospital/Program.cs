31da393c4942f1faba334ae0922baa8f81ad910c
Jonas <jonasarvad2@gmail.com>
2020-05-18 13:46:53 +0200

    ny ændreing
    ehj

--- AnimalHospital/Program.cs
+++ AnimalHospital/Program.cs
@@ -40,7 +40,7 @@ namespace AnimalHospital
             }
             else if (k == '4')
             {
-                Console.WriteLine("Not yet implemented!");
+                WriteLine("Not yet implemented!");
             }
             else if (k == '5')
             {
